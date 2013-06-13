15124161f5890ac4d10a9d8efeaf8f67a94855e9
jarrieta <jarrieta@newbay.com>
2013-06-13 15:04:22 +0100

    property-register-6 ArrayIndexOutOfBounds when GET /property/{id} has no match

--- records-rest/src/main/scala/org/techdelivery/property/spray/PropertyResource.scala
+++ records-rest/src/main/scala/org/techdelivery/property/spray/PropertyResource.scala
@@ -1,6 +1,6 @@
 package org.techdelivery.property.spray
 
-import akka.actor.Actor
+import akka.actor.{ActorLogging, Actor}
 import spray.http.{HttpBody, HttpResponse, HttpRequest}
 import spray.http.HttpHeaders._
 import spray.http.HttpMethods._
@@ -14,7 +14,7 @@ import org.techdelivery.property.entity._
 import RecordMapper._
 import RegistryRecordProtocol._
 
-class PropertyResource(collection: BSONCollection) extends Actor {
+class PropertyResource(collection: BSONCollection) extends Actor with ActorLogging {
   val get_rx = "^\\/property\\/(\\w*)$".r
 
   def receive= {
@@ -22,17 +22,23 @@ class PropertyResource(collection: BSONCollection) extends Actor {
       val origin = sender
       path match {
         case get_rx(id) => {
-          val filter = BSONDocument( "_id" -> BSONObjectID(id))
-          val result = collection.find(filter).cursor[MongoRegistryRecord]
-          val response = result.toList
-          response onComplete {
-            case Success(list) => {
-              list match {
-                case record :: Nil => origin ! HttpResponse( status = 200, entity = HttpBody(`application/json`,record.toJson.toString) )
-                case Nil => origin ! HttpResponse( status = 404 )
+          try {
+            val filter = BSONDocument( "_id" -> BSONObjectID(id))
+            val result = collection.find(filter).cursor[MongoRegistryRecord]
+            val response = result.toList
+            response onComplete {
+              case Success(list) => {
+                list match {
+                  case record :: Nil => origin ! HttpResponse( status = 200, entity = HttpBody(`application/json`,record.toJson.toString) )
+                  case record :: xs => log.error("Get " + collection.name + "[" + id + "] returned " + list.size + " matches instead of 1")
+                  case Nil => origin ! HttpResponse( status = 404 )
+                }
               }
-            } 
-            case Failure(f) => origin ! HttpResponse( status = 503, entity = f.getMessage)
+              case Failure(f) => origin ! HttpResponse( status = 503, entity = f.getMessage)
+            }
+          } catch {
+            case iae: IllegalArgumentException => origin ! HttpResponse(status = 404)
+            case e: Exception => { log.warning(e.getMessage) ; log.debug(e.getMessage, e) ; origin ! HttpResponse(status = 500) }
           }
         }
         case _ => origin ! HttpResponse(status = 404)
