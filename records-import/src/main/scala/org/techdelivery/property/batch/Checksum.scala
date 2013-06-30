db80c6d28a89bbdd314fe76dcf53b0b0bbbd8908
javier <javier@techdelivery.es>
2013-06-30 18:40:46 +0100

    property-register-10 Create importer actor that will insert a record in the database

--- records-import/src/main/scala/org/techdelivery/property/batch/Checksum.scala
+++ records-import/src/main/scala/org/techdelivery/property/batch/Checksum.scala
@@ -11,7 +11,8 @@ import scala.concurrent.ExecutionContext.Implicits._
 import scala.util.Success
 import scala.util.Failure
 import com.typesafe.scalalogging.slf4j.Logging
-import org.techdelivery.property.entity.RegistryRecord
+import org.techdelivery.property.entity.{RegistryRecordLineParser, RegistryRecord}
+import akka.actor.{ActorRef, Actor, ActorLogging}
 
 case class Checksum(id:Option[String], checksum: Array[Byte]) {
   def toHex(b: Array[Byte]): String = b.map{b => String.format("%02X", java.lang.Byte.valueOf(b))} mkString("")
@@ -31,32 +32,46 @@ object Checksum extends Logging {
   }
 }
 
-object ChecksumChecker extends Logging {
+class ChecksumCheckerActor(importer: ActorRef) extends Actor with ActorLogging {
   private def col = checksumCollection
-  
+
+  def receive = {
+    case line: List[String] => {
+      try {
+        val csum = Checksum(line)
+        val record = RegistryRecordLineParser(line)
+        runAfterCheck(csum, record)
+      } catch {
+        case e: IllegalArgumentException => log.warning(line + ": " + e.getMessage)
+      }
+    }
+
+    case m => log warning("Unknown line " + m.toString)
+  }
+
   implicit def toBinary(d: Array[Byte]): BSONBinary = new BSONBinary(ArrayReadableBuffer(d), Subtype.Md5Subtype)
-  
-  def runAfterCheck(sum: Checksum, record: RegistryRecord, f: RegistryRecord => Unit) {
+
+  def runAfterCheck(sum: Checksum, record: RegistryRecord) {
     val cursor = col.find(BSONDocument("checksum" -> toBinary(sum.checksum))).cursor[BSONDocument].toList
     cursor onComplete {
       case Success(list) => {
         list match {
-          case Nil => insertChecksum(sum, record)(f)
-          case _ => logger.error("Line " + record + " already imported, ignoring")
+          case Nil => insertChecksum(sum, record)
+          case _ => log.error("Line " + record + " already imported, ignoring")
         }
       }
-      case Failure(t) => { logger.error(t.getMessage); logger.debug(t.getMessage, t)}
+      case Failure(t) => { log.error(t.getMessage); log.debug(t.getMessage, t)}
     }
   }
-  
-  private def insertChecksum(sum: Checksum, record: RegistryRecord)(f: RegistryRecord => Unit): Unit = {
+
+  private def insertChecksum(sum: Checksum, record: RegistryRecord)= {
     val result = col.insert(BSONDocument("checksum" -> toBinary(sum.checksum)))
     result onComplete {
-      case Success(op) => { 
-        logger.debug("Added checksum: " + sum)
-        f(record)
+      case Success(op) => {
+        log.debug("Added checksum: " + sum)
+        importer ! record
       }
-      case Failure(t) => { logger.error(t.getMessage); logger.debug(t.getMessage, t)}
+      case Failure(t) => { log.error(t.getMessage); log.debug(t.getMessage, t)}
     }
   }
 }
--- records-import/src/main/scala/org/techdelivery/property/batch/ImporterApp.scala
+++ records-import/src/main/scala/org/techdelivery/property/batch/ImporterApp.scala
@@ -3,36 +3,26 @@ package org.techdelivery.property.batch
 import scala.io.Source
 import scala.io.Codec
 import org.techdelivery.property.batch.parser.csvParser
-import akka.actor.Props
-import org.techdelivery.property.entity.{RegistryRecord, RegistryRecordLineParser}
+import akka.actor.{PoisonPill, Props}
 import org.techdelivery.property.mongo.MongoImporter
 import org.techdelivery.property.mongo.propertyMongoDB._
 import com.typesafe.scalalogging.slf4j.Logging
 
 object ImporterApp extends App with Logging {
 
-  def registerLine(rec: RegistryRecord): Unit = { importer !  rec }
-  
+  //Initialize actors
   val system = actorSystem
-  
   val importer = system.actorOf(Props(new MongoImporter(propertyCollection)))
+  val recordParser = system.actorOf(Props(new ChecksumCheckerActor(importer)))
   
   args.foreach { importRegistries(_) }
 
-  system.shutdown()
+  recordParser ! PoisonPill
   
   def importRegistries(file: String) = {
     val lines = Source.fromFile(file)(Codec.ISO8859).getLines().drop(1)
     val records = lines.map(csvParser.parse(_)(0))
-    records.foreach { line =>
-      try {
-        val record = RegistryRecordLineParser(line)
-        val csum = Checksum(line)
-        ChecksumChecker.runAfterCheck(csum, record, registerLine )
-      } catch {
-        case e: IllegalArgumentException => logger.warn(line + ": " + e.getMessage)
-      }
-    }
+    records.foreach { recordParser ! _ }
   }
   
 }
