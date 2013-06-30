f1c450883df43956fbb267db696a2c6f90aeb235
javier <javier@techdelivery.es>
2013-06-30 22:29:48 +0100

    Created a new ActorSystem to provide resources for app related actors, separated from the Mongo ActorSystem

--- records-import/src/main/scala/org/techdelivery/property/batch/ImporterApp.scala
+++ records-import/src/main/scala/org/techdelivery/property/batch/ImporterApp.scala
@@ -3,24 +3,23 @@ package org.techdelivery.property.batch
 import scala.io.Source
 import scala.io.Codec
 import org.techdelivery.property.batch.parser.csvParser
-import akka.actor.{PoisonPill, Props}
+import akka.actor.{ActorSystem, Props}
 import org.techdelivery.property.mongo.MongoImporter
-import org.techdelivery.property.mongo.propertyMongoDB
 import org.techdelivery.property.mongo.propertyMongoDB._
 import com.typesafe.scalalogging.slf4j.Logging
 
 object ImporterApp extends App with Logging {
 
   //Initialize actors
-  val system = actorSystem
+  val system = ActorSystem("importer")
   val importer = system.actorOf(Props(new MongoImporter(propertyCollection)))
   val recordParser = system.actorOf(Props(new ChecksumCheckerActor(importer)))
   
   args.foreach { importRegistries(_) }
 
-  recordParser ! PoisonPill
-  importer ! PoisonPill
-  propertyMongoDB shutdown
+//  recordParser ! PoisonPill
+//  importer ! PoisonPill
+//  propertyMongoDB shutdown
   
   def importRegistries(file: String) = {
     val lines = Source.fromFile(file)(Codec.ISO8859).getLines().drop(1)
